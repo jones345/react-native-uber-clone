e15833ab4e1542f40fcc3401890d923335268f0c
zntongana <zntongana@bitri.co.bw>
2021-08-26 12:25:15 +0200

    update navigation

--- App.js
+++ App.js
@@ -5,13 +5,31 @@ import HomeScreen from "./screens/HomeScreen";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
 import {store} from "./store"
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
       <Provider store={store}>
+        <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen/>
+        <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} 
+        options={{
+          headerShown:false,
+          
+        }}
+        />
+          <Stack.Screen name="MapScreen" component={MapScreen} 
+        options={{
+          headerShown:false,
+          
+        }}
+        />
+      </Stack.Navigator>
         </SafeAreaProvider>
+        </NavigationContainer>
       </Provider>
   );
 }
